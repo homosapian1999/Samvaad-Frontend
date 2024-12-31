2bc73a2954e9f9e9ff82d91ced75e3575527357c
homosapian <ankitd349@gmail.com>
2024-12-31 22:00:48 +0530

    Add Auth UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 // import { Button } from "./components/ui/button";
 
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Auth from "./pages/auth/Auth";
+import Chat from "./pages/chat/Chat";
+import Profile from "./pages/profile/Profile";
+
 const App = () => {
   return (
-    <>
-      <h1>Hello Ankit</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 

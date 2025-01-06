059152f57d683ad34473210a48719c57d86eda61
homosapian <ankitd349@gmail.com>
2025-01-06 23:58:21 +0530

    Add Profile image and api integrations

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,5 +1,5 @@
 import { useAppStore } from "@/store/store";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoArrowBack } from "react-icons/io5";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
@@ -9,22 +9,32 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { apiClient } from "@/lib/auth-client";
-import { UPDATE_PROFILE_ROUTE } from "@/utils/constants";
+import {
+  HOST,
+  PROFILE_IMAGE_ROUTE,
+  REMOVE_PROFILE_IMAGE_ROUTE,
+  UPDATE_PROFILE_ROUTE,
+} from "@/utils/constants";
 
 const Profile = () => {
+  // window.location.reload();
   const navigate = useNavigate();
   const { userInfo, setUserInfo } = useAppStore();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState<string | ArrayBuffer | null>(null);
   const [hovered, setHovered] = useState(false);
   const [selectedColor, setSelectedColor] = useState(0);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    if (userInfo?.profileSetup) {
+    if (userInfo?.profileSetup || userInfo?.firstName) {
       setFirstName(userInfo?.firstName as string);
       setLastName(userInfo?.lastName as string);
       setSelectedColor(userInfo?.color as number);
+    }
+    if (userInfo?.image) {
+      setImage(`${HOST}/${userInfo.image}`);
     }
   }, [userInfo]);
 
@@ -65,10 +75,56 @@ const Profile = () => {
     }
   };
 
+  const handleNavigate = () => {
+    if (userInfo?.profileSetup) {
+      // navigate("/chat");
+    } else {
+      toast.error("Please complete the profile");
+    }
+  };
+
+  const handleFileInputClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleImageChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("profile-image", file);
+      const response = await apiClient.post(PROFILE_IMAGE_ROUTE, formData, {
+        withCredentials: true,
+      });
+      if (response.status) {
+        setUserInfo({ ...userInfo, image: response.data.image });
+        toast.success("Image Updates Successfully");
+      }
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDeleteImage = async () => {
+    const response = await apiClient.delete(REMOVE_PROFILE_IMAGE_ROUTE, {
+      withCredentials: true,
+    });
+    if (response.status) {
+      setUserInfo({ ...userInfo, image: null as unknown as string });
+      toast.success("Image removed successfully");
+    }
+  };
+
   return (
     <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
       <div className="flex flex-col gap-10 w-[80vw] md:w-max">
-        <div>
+        <div onClick={handleNavigate}>
           <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
         </div>
         <div className="grid grid-cols-2">
@@ -80,7 +136,7 @@ const Profile = () => {
             <Avatar className="h-32 w-32 md:w-48 md:h-48 rounded-full overflow-hidden ">
               {image ? (
                 <AvatarImage
-                  src={image}
+                  src={image as string}
                   alt="profile"
                   className="object-cover w-full h-full bg-black"
                 />
@@ -97,7 +153,10 @@ const Profile = () => {
               )}
             </Avatar>
             {hovered && (
-              <div className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full">
+              <div
+                className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full"
+                onClick={image ? handleDeleteImage : handleFileInputClick}
+              >
                 {image ? (
                   <FaTrash className="text-white text-3xl cursor-pointer" />
                 ) : (
@@ -105,6 +164,14 @@ const Profile = () => {
                 )}
               </div>
             )}
+            <input
+              type="file"
+              ref={fileInputRef}
+              className="hidden"
+              onChange={handleImageChange}
+              name="profile-image"
+              accept=".png, .jpg, .jpeg, .svg, .webp"
+            />
           </div>
           <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
             <div className="w-full">
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,5 @@ export const SIGNUP_ROUTE = `${AUTH_ROUTES}/register`;
 export const LOGIN_ROUTE = `${AUTH_ROUTES}/login`;
 export const GET_USER_INFO = `${AUTH_ROUTES}/user-info`;
 export const UPDATE_PROFILE_ROUTE = `${AUTH_ROUTES}/update-profile`;
+export const PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/add-profile-image`;
+export const REMOVE_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/remove-profile-image`;

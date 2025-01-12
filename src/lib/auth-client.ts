54457854d3ea367879157852ff1776f3f7da0218
homosapian <ankitd349@gmail.com>
2025-01-12 11:00:54 +0530

    Configure for deployment

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "samvaad-backend-7r2qzq091-homosapian1999s-projects.vercel.app",
+  baseURL:
+    "https://samvaad-backend-7r2qzq091-homosapian1999s-projects.vercel.app",
   withCredentials: true,
 });

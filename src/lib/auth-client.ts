2389f9a7799730d08ed326b2bc1d6ad746d14417
Ankit Dubey <52074379+homosapian1999@users.noreply.github.com>
2025-01-12 12:01:51 +0530

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://samvaad-backend.vercel.app",
   withCredentials: true,
 });
 
